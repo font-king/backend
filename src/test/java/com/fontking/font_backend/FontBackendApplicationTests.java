05372d532f5e2cc59ce46d4dff3dea7f30f4d955
JeongHoon Lee <sherry0_@kakao.com>
2024-12-19 14:15:30 +0900

    init

---
+++ src/test/java/com/fontking/font_backend/FontBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fontking.font_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FontBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
